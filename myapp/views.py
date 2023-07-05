4f4319aef3cc97daba429e8dbf6238aef6033ff7
MZak2206 <mateusz.zak2206@gmail.com>
2023-07-05 09:46:51 +0200

    Added graph and fixed layout

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,15 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render
-from django.http import HttpResponse
-import requests
-from datetime import datetime, timedelta
 import locale
+from datetime import datetime, timedelta
 
+import matplotlib.pyplot as plt
+import requests
+from babel.dates import format_date, format_datetime, format_time
+from django.http import HttpResponse
+from django.shortcuts import render
+from matplotlib import dates as mpl_dates
+from matplotlib.ticker import FuncFormatter
+import matplotlib
+matplotlib.use('Agg')
 
 def weather(request):
     city = "Łódź"
@@ -17,18 +22,18 @@ def get_weather_data(city):
     api_key = "5acd2a90879682f6a0023ad1a75ff7df"    
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&lang=pl&units=metric"
     
-    
     response = requests.get(url)
 
     locale.setlocale(locale.LC_TIME, 'pl_PL.UTF-8')
     if response.status_code == 200:
         data = response.json()
         weather_data = []
+        dates = []
+        temperatures = []
+        
         today = datetime.now().date()
         for i in range(5):
             date = today + timedelta(days=i)
-            # day = date.strftime("%A")
-            # print(day)
             weather_info = {
                 "city": city,
                 "day": date,
@@ -38,6 +43,32 @@ def get_weather_data(city):
                 "icon" : data["list"][i]["weather"][0]["icon"]
             }
             weather_data.append(weather_info)
+            
+            dates.append(date)
+            temperatures.append(weather_info["temperature"])
+            
+        plt.plot(dates, temperatures)
+        plt.xlabel('Data')
+        plt.ylabel('Temperatura (°C)')
+        plt.title('Wykres temperatury')
+        
+        # Formatowanie osi czasu
+        def get_weekday(x, pos=None):
+            
+            date =  format_date(dates[pos], format='d/M/yyyy', locale='pl_PL')
+            weekday =  format_date(dates[pos], format='EEEE', locale='pl_PL')
+            return str(date+"\n"+weekday)
+        
+        plt.gca().xaxis.set_major_formatter(FuncFormatter(get_weekday))
+        plt.gca().xaxis.set_major_locator(mpl_dates.DayLocator())
+        plt.gcf().autofmt_xdate()  # Automatyczne dostosowanie etykiet osi X
+
+        
+        plt.tight_layout()  # Opcjonalne - poprawia układ wykresu
+
+        plt.savefig('static/wykres.png')  # Zapisz wykres do pliku (opcjonalnie)
+        plt.close()  # Zamknij wykres, aby zwolnić pamięć
+        
         return weather_data
     else:
         return []
